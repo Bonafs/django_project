5749edf5ba119fdd8a6cc3163069528d529d868f
Bonafs <bonafs@yahoo.com>
2025-06-26 12:34:22 +0100

    Add Procfile and specify gunicorn within it, add gunicorn to requirements.txt, add herokuapp.com and 127.0.0.1 to allowed hosts in settings.py and also set DEBUG to False

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i7s*raxl&cj+^9qx-7zhqjbdap6@2m4jkw)fm&f1#%456+kyb=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
